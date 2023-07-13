0dd1bba437c2accb04fdefb72df636ad772be10e
gihanthakavishan <kavisha-se19048@stu.ac.kln>
2023-07-13 19:36:45 +0530

    updated_files

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const nameError = document.getElementById('nameError');
 submitBtn.addEventListener('click',(e)=>{
    e.preventDefault();
 
-   if(validateName()){
+   if(validateName() && validateEmail){
       alert("Form submitted Successfully");
    }
 });
@@ -14,12 +14,33 @@ function validateName(){
 
     if(name.length == 0){
          nameError.innerHTML = "Name is required"
+         nameError.previousElementSibling.classList.add('fa-xmark');
          return false;
     }
 if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
     nameError.innerHTML = "Write full Name";
+    nameError.previousElementSibling.classList.add('fa-xmark');
     return false;
 }
     nameError.innerHTML = "";
+    nameError.previousElementSibling.classList.add('fa-check');
+    return true;
+}
+
+function validateEmail(){
+    let email = document.getElementById('email').value;
+
+    if(email.length == 0){
+         emailError.innerHTML = "Email is required"
+         emailError.previousElementSibling.classList.add('fa-xmark');
+         return false;
+    }
+if(!email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)){
+    emailError.innerHTML = "Provide valid Email";
+    emailError.previousElementSibling.classList.add('fa-xmark');
+    return false;
+}
+    emailError.innerHTML = "";
+    emailError.previousElementSibling.classList.add('fa-check');
     return true;
 }
